a37295a1c05b28a9d7668aaaffaea3649bfe7168
Vivek Sharma (viveksh2) <viveksh2@cisco.com>
2018-04-16 16:37:51 -0700

    The services code checkin.

---
+++ 02_Angular_Class_2/02_finish/ucsc-services-demo/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+import {Routes, RouterModule} from "@angular/router";
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'find', redirectTo: 'search'},
+  {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'recipeDetail/:recipeID', component: RecipeDetailComponent },
+  {path: '**', component: HomeComponent}
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    RecipeDetailComponent,
+    RegisterComponent,
+    SearchComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash:true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
