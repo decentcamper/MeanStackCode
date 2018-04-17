d8bf78191cb7c21e83464a0195abf06b9b91b822
Vivek Sharma (viveksh2) <viveksh2@cisco.com>
2018-04-16 17:55:29 -0700

    The services code checkin.

--- 02_Angular_Class_2/02_finish/ucsc-services-demo/src/app/components/search/search.component.ts
+++ 02_Angular_Class_2/02_finish/ucsc-services-demo/src/app/components/search/search.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import {RecipeDetails} from "../../models/recipe-details";
+import {Recipes} from "../../models/recipes";
+import {RecipeDetailsService} from "../../services/recipe-details.service";
+
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [RecipeDetailsService]
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  recipes : RecipeDetails[];
+
+  constructor(private recipeService: RecipeDetailsService) { }
+
+
+  getRecipes() : void {
+    this.recipeService.getRecipes().then(movies =>{
+        this.recipes = movies
+       console.log(this.recipes)
+    }); // the TS allows this nice little syntax to acheive this
+  }
+
 
   ngOnInit() {
+      this.getRecipes();
+
   }
 
 }
